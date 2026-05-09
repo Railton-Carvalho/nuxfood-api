cc69ffe5b2eb8b3ce7ce0f145c19eec09a1332d8
Railton <railtonplayrx@gmail.com>
2026-05-09 10:19:40 -0300

    Init Project

---
+++ src/main/java/com/nuxfood/pedidos/NuxfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.nuxfood.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuxfoodApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuxfoodApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nuxfood/pedidos/NuxfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuxfood.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuxfoodApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

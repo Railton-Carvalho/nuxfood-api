5729deb04fbe68da6d80bdddd3850be6d43e84b7
Railton <railtonplayrx@gmail.com>
2026-05-09 10:22:43 -0300

    feat: add main models order and orderStatus

---
+++ src/main/java/com/nuxfood/pedidos/model/Order.java
@@ -0,0 +1,22 @@
+package com.nuxfood.pedidos.model;
+
+import com.nuxfood.pedidos.model.enums.OrderStatus;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private String orderId;
+    private String userId;
+    private String product;
+    private Double total;
+    private OrderStatus status;
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/nuxfood/pedidos/model/enums/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.nuxfood.pedidos.model.enums;
+
+public enum OrderStatus {
+    CREATED,
+    CONFIRMED,
+    PREPARING,
+    OUT_FOR_DELIVERY,
+    DELIVERED,
+    CANCELLED,
+    PAID
+}
